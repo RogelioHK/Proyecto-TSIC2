8f107bece3213fd60155434d8ff1ee6a08af0d5c
Eduardo4d4 <eduardo.tapia4d4@gmail.com>
2023-06-14 12:38:09 -0600

    Actualizacion
    Modifique html le di mas sentido a la idea y ordene las imagenes
    + el scrip de crear BD
    Agregue un txt con una carga inicial en Contenedor

--- Pagina/registro.php
+++ Pagina/registro.php
@@ -1,65 +1,70 @@
 <?php
-// Verificar si se envió el formulario de registro
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtener los datos enviados del formulario
-    $email = $_POST["email"];
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    
-    // Validar los datos
-    if (empty($email) || empty($username) || empty($password)) {
-        echo "Todos los campos son obligatorios";
-        exit;
-    }
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "El correo electrónico no es válido";
-        exit;
-    }
-
-		// Validar el nombre de usuario: solo letras y números
-		if (!preg_match("/^[a-zA-Z0-9]+$/", $username)) {
-				echo "El nombre de usuario solo puede contener letras y números";
-				exit;
+echo '<style>
+		body {
+			background-color: #F9D0D9;
 		}
+	</style>';
+// Configuración de la conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "316170040";
+$dbname = "teamsoft";
 
-		// Validar la contraseña: al menos una letra y un número, longitud mínima de 8 caracteres
-		if (!preg_match("/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/", $password)) {
-				echo "La contraseña debe contener al menos una letra y un número, y tener una longitud mínima de 8 caracteres";
-				exit;
-		}
-    // Sanitizar los datos
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    $username = filter_var($username, FILTER_SANITIZE_STRING);
-    // No se necesita sanitizar la contraseña, ya que se utilizará en una consulta preparada (prepared statement) más adelante.
+// Obtener los datos enviados del formulario
+$email = $_POST["email"];
+$nombre = $_POST["username"];
+$recipe = $_POST["recipe"];
 
-
-    // Procesar los datos
-		// Conexión a la base de datos MySQL
-		$servername = "mysql-service";
-		$username = "root";
-		$password = "password";
-		$port = 3306;
-		$dbname = "clientes";
-
-		// Crear conexión
-		$conn = new mysqli($servername, $username, $password, $dbname,$port);
-
-		// Verificar la conexión
-		if ($conn->connect_error) {
-				die("Se produjo un problema al establecer conexión con la base de datos. " . $conn->connect_error);
-		}
-
-		// Insertar los datos en la tabla
-		$sql = "INSERT INTO cliente (correo, nombre, contraseña) VALUES ('$email', '$username', '$password')";
-
-		if ($conn->query($sql) === TRUE) {
-				echo "Datos ingresados correctamente.";
-		} else {
-				echo "No se pudieron ingresar los datos correctamente. Por favor, revisa los valores ingresados. " . $conn->error;
-		}
-
-		// Cerrar la conexión
-		$conn->close();
+// Validar los datos
+if (empty($email) || empty($nombre) || empty($recipe)) {
+	echo "Todos los campos son obligatorios";
+	exit;
 }
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	echo "El correo electrónico no es válido";
+	exit;
+}
+
+// Validar el nombre de usuario: solo letras y números
+if (!preg_match("/^[a-zA-Z0-9]+$/", $nombre)) {
+	echo "El nombre de usuario solo puede contener letras y números";
+	exit;
+}
+
+// Validar la receta: máximo 1000 caracteres
+if (strlen($recipe) > 1000) {
+	echo "La receta no puede exceder los 1000 caracteres";
+	exit;
+}
+
+// Sanitizar los datos
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+// No se necesita sanitizar la receta, ya que se utilizará en una consulta preparada (prepared statement) más adelante.
+
+// Procesar los datos
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+	die("Se produjo un problema al establecer conexión con la base de datos. " . $conn->connect_error);
+}
+
+// Insertar los datos en la tabla
+$sql = "INSERT INTO cliente (correo, nombre, receta) VALUES ('$email', '$nombre', '$recipe')";
+
+
+// Ejecutar la consulta
+if ($conn->query($sql) === TRUE) {
+    echo "Datos guardados correctamente";  <a href='index.html'>Regresar</a></center>";
+} else {
+    echo "Error al guardar los datos: " . $conn->error;
+}
+
+// Cerrar la conexión
+$conn->close();
+
 ?>
