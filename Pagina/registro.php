6d442fa7520ae14ca50f1c39d4745330846489e8
Hernández Ku Rogelio <rogherku@comunidad.unam.mx>
2023-06-15 20:17:41 -0600

    Create Container
    Corrige el archivo registro.php para que pueda redireccionar a la página de inicio cuando se hace añade una receta a la base de datos
    Crea la tabla correspondiente a través de setup.sql
    Corrige el Dockerfile para que pueda crear el contenedor correctamente

--- Pagina/registro.php
+++ Pagina/registro.php
@@ -1,70 +1,33 @@
 <?php
-echo '<style>
-		body {
-			background-color: #F9D0D9;
-		}
-	</style>';
-// Configuración de la conexión a la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "316170040";
-$dbname = "teamsoft";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Obtener los valores enviados desde el formulario
+    $email = $_POST['email'];
+    $name = $_POST['username'];
+    $recipe = $_POST['recipe'];
 
-// Obtener los datos enviados del formulario
-$email = $_POST["email"];
-$nombre = $_POST["username"];
-$recipe = $_POST["recipe"];
+    //Valores para la conexión con la base de datos
+    $host = 'localhost';
+    $database = 'teamsoft';
+    $username = 'root';
+    $password = '316170040';
 
-// Validar los datos
-if (empty($email) || empty($nombre) || empty($recipe)) {
-	echo "Todos los campos son obligatorios";
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	//Envía los datos recibidos a la base de datos
+        $stmt = $conn->prepare("INSERT INTO recetas (email, username, recipe) VALUES (:email, :username, :recipe)");
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':username', $name);
+        $stmt->bindParam(':recipe', $recipe);
+        $stmt->execute();
+	//Retorna a la pantalla de inicio
+	header("Location: index.html");
 	exit;
-}
-
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-	echo "El correo electrónico no es válido";
-	exit;
-}
-
-// Validar el nombre de usuario: solo letras y números
-if (!preg_match("/^[a-zA-Z0-9]+$/", $nombre)) {
-	echo "El nombre de usuario solo puede contener letras y números";
-	exit;
-}
-
-// Validar la receta: máximo 1000 caracteres
-if (strlen($recipe) > 1000) {
-	echo "La receta no puede exceder los 1000 caracteres";
-	exit;
-}
-
-// Sanitizar los datos
-$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
-// No se necesita sanitizar la receta, ya que se utilizará en una consulta preparada (prepared statement) más adelante.
-
-// Procesar los datos
-
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-	die("Se produjo un problema al establecer conexión con la base de datos. " . $conn->connect_error);
-}
-
-// Insertar los datos en la tabla
-$sql = "INSERT INTO cliente (correo, nombre, receta) VALUES ('$email', '$nombre', '$recipe')";
-
-
-// Ejecutar la consulta
-if ($conn->query($sql) === TRUE) {
-    echo "Datos guardados correctamente";  <a href='index.html'>Regresar</a></center>";
+    } catch(PDOException $e) {
+        echo "Error al registrar la receta: " . $e->getMessage();
+    }
 } else {
-    echo "Error al guardar los datos: " . $conn->error;
+    echo "Acceso denegado";
 }
-
-// Cerrar la conexión
-$conn->close();
-
 ?>
+
